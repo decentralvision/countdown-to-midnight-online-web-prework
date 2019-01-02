98d4e60923775502dce06f245cc0b06412e9fb7d
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-02 04:15:14 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,11 @@
 #write your code here
 
 def countdown(num)
-  if num == 12
-    return "HAPPY NEW YEAR!"
-  else
-    while num > 0
-      puts "#{num} SECOND(S)!"
-      num -= 1
-    end
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
   end
+  return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(num)
