6c799248a4e3db0cec9db6d47e1c134d1c8887ec
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-02 04:16:10 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -5,13 +5,14 @@ def countdown(num)
     puts "#{num} SECOND(S)!"
     num -= 1
   end
-  return "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(num)
-  sleep(5)
   while num > 0
     puts "#{num} SECOND(S)!"
+    sleep(1)
     num -= 1
   end
+  "HAPPY NEW YEAR!"
 end
